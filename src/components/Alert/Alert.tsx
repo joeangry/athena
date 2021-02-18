b4abae10948feb26790ccdecd065615474164b70
joeangry <tobias.kammersgaard@gmail.com>
2021-02-18 16:55:14 +0100

    Update Alert.tsx

--- src/components/Alert/Alert.tsx
+++ src/components/Alert/Alert.tsx
@@ -1,16 +1,20 @@
 import React from "react";
 import "./Alert.scss";
 
-export type alertType = "primary" | "light" | "info" | "error" | "warn";
+export type AlertType = "Primary" | "Secondary" | "Success" | "Danger" | "Warning" | "Info" | "Light" | "Dark";
 
 export interface IAlertProps {
     text: string;
-    type: alertType;
+    type: AlertType;
     additionalClasses?: string;
+    canDismiss: boolean;
+    dismissText?: string;
 }
 
 export const Alert = (props: IAlertProps) => {
     return (<div className={`alert alert-${props.type}${props.additionalClasses ? ` ${props.additionalClasses}` : ""}`} role="alert">
+    return (<div className={`alert alert-${props.type.toLowerCase()}${props.additionalClasses ? ` ${props.additionalClasses}` : ""}`} role="alert">
         {props.text}
+        }
       </div>);
 }
