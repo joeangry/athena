a6c6b1a38f7bffb33179d9b758da64d20a2fe8de
joeangry <tobias.kammersgaard@gmail.com>
2021-03-01 17:02:52 +0100

    Basic spinner

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -3,9 +3,12 @@ import "./Spinner.scss";
 
 export type SpinnerType = "Primary" | "Secondary" | "Success" | "Danger" | "Warning" | "Info" | "Light" | "Dark";
 
+export type SpinnerSize = "Small" | "Medium" | "Large";
+
 export interface ISpinnerProps {
     type: SpinnerType;
     text?: string;
+    size?: SpinnerSize;
 }
 
-export const Spinner = (props: ISpinnerProps) => (<div className={`spinner-container ${props.type.toLowerCase()}`}></div>)
+export const Spinner = (props: ISpinnerProps) => (<div className={`spinner-container ${props.type.toLowerCase()} spinner-${props.size}`}></div>);
