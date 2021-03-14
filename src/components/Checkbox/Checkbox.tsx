7f03b2bb5a209945563edb69e339cd8f5bf9baf2
joeangry <tobias.kammersgaard@gmail.com>
2021-03-14 21:17:36 +0100

    Update Checkbox.tsx

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -1,28 +1,20 @@
-interface IProps {
-    /**
-     * Class name to provide on the root element, in addition to the `ms-Checkbox` class.
-     */
+import React from "react";
+import "./Checkbox.scss";
+
+interface ICheckboxProps {
     className?: string;
-    /**
-      * Checked state. Mutually exclusive with `defaultChecked`. Use this if you control the checked state at a higher
-      * level and plan to pass in the correct value based on handling `onChange` events and re-rendering.
-      */
     checked?: boolean;
-    
     label?: string;
-
     name?: string;
-
     disabled?: boolean;
-
-    onChange: (event: any) => void;
+    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export const Checkbox = (props: IProps) => {
-    return (<>
-        <input type="checkbox" name={props.name} checked={props.checked} />
-        <label>
-            {props.label}
-        </label>f
+export const Checkbox = (props: ICheckboxProps) => {
+    return (<>       
+        <label className="container">{props.label}
+            <input type="checkbox" checked={props.checked} />
+            <span className="checkmark"></span>
+        </label>
     </>)
 }
