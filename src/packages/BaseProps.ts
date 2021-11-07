af0ca15d8be99e46daf7f78386cae562f19d87eb
joeangry <tobias.kammersgaard@gmail.com>
2021-11-07 12:23:37 +0100

    Base types

---
+++ src/packages/BaseProps.ts
@@ -0,0 +1,6 @@
+import { ColorBlindMode, WcagLevel } from "./types";
+
+export class BaseProps {
+    colorBlind: ColorBlindMode;
+    ariaLevel: WcagLevel;
+}
---
+++ src/packages/types.ts
@@ -0,0 +1,2 @@
+export type WcagLevel = "A" | "AA" | "AAA";
+export type ColorBlindMode = "Protanopia" | "Deuteranopia" | "Protanomaly" | "Deuteranomaly";
