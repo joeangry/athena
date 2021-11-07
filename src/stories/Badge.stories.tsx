5a69ac2d9cd1f1c1a7ea1569b10330f229c9a442
joeangry <tobias.kammersgaard@gmail.com>
2021-11-07 14:00:31 +0100

    Badge stories

--- src/stories/Badge.stories.tsx
+++ src/stories/Badge.stories.tsx
@@ -1,18 +1,47 @@
 import React from "react";
-import { Badge, IBadgeProps as BadgeProps } from "../components/Badge/Badge.tsx";
+import { Badge, IBadgeProps as BadgeProps, BadgeType } from "../components/Badge/Badge.tsx";
 
 const Template = (args: BadgeProps) => (<Badge {...args} />);
 
-export const CountBadge = Template.bind({});
-CountBadge.args = {
+export const PrimaryBadge = Template.bind({});
+PrimaryBadge.args = {
     text: "500",
-    bgColor: "bg-secondary",
     type: "Primary"
+};
+export const SecondaryBadge = Template.bind({});
+SecondaryBadge.args = {
+    text: "100%",
+    type: "Secondary"
+};
+
+export  const Success = Template.bind({});
+Success.args = {
+    text: "Success",
+    type: "Success"
+};
+
+export const Danger = Template.bind({});
+Danger.args = {
+    text: "Danger!",
+    type: "Danger"
+};
+
+export const Warning = Template.bind({});
+Warning.args = {
+    text: "Warning!",
+    type: "Warning"
+};
+
+export const Info = Template.bind({});
+Info.args = {
+    text: "Information!",
+    type: "Info"
 };
 
 const defaultExport = {
     title: "Components/Badge",
     component: Badge,
 };
+
 
 export default defaultExport;
