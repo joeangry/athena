7ad5c668a54ecdc3de814bfa480c7ee54cf244a7
joeangry <tobias.kammersgaard@gmail.com>
2025-12-23 13:47:27 +0100

    Upgrade storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -13,8 +13,8 @@ module.exports = {
     "addons": [
         "@storybook/addon-a11y",
         "@storybook/addon-links",
-        "@storybook/addon-essentials",
-        "@storybook/preset-create-react-app"
+        "@storybook/preset-create-react-app",
+        "@storybook/addon-docs"
     ],
     framework: {
         name: "@storybook/react-webpack5",
